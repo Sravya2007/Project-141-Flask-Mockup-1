624ea8ca305694e19439ab03fa027dd5045aa49c
Sravya <65549677+Sravya2007@users.noreply.github.com>
2021-07-20 21:15:56 +0530

    Flask Mockup-1

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ with open('articles.csv', encoding = "utf-8") as f:
     csv_reader = csv.reader(f)
     article_data = list(csv_reader)
     column_names = article_data[0]
-    for values in article_data[1:].strip:
-        for value in values:
-            value.strip("\\")
+    for values in article_data[1:]:
         all_articles.append(dict(zip(column_names, values)))
 
 liked_articles = []
